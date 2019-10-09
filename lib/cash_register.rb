d19031555cef668e3dc96c7c6468e8164dcd9d2b
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-09 21:50:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :add_item #return current total when enter instance.total
+  
+  def initialize(discount = 0.20)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += ( price * quantity)
+  end
+  
+  def apply_discount
+    # binding.pry
+    total_in_float = @total * (1 - @discount)
+    @total = total_in_float.to_i
+  end
+  
+end
