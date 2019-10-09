e2fe41c256797d7483378ed91377eff4378a5674
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-09 22:10:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,40 +13,22 @@ class CashRegister
   end
   
   def apply_discount
-    # binding.pry
+    return "There is no discount to apply." unless @discount
+    discount = @total * @discount / 100 
+    @total = @total - discount
+    return "After the discount, the total comes to $#{@total}."
     
-    if @discount
-      discount = @total * @discount / 100 
-      @total = @total - discount
-      return "After the discount, the total comes to $#{@total}."
-    else
-      return "There is no discount to apply."
-    end
+    # if @discount
+    #   discount = @total * @discount / 100 
+    #   @total = @total - discount
+    #   return "After the discount, the total comes to $#{@total}."
+    # else
+    #   return "There is no discount to apply."
+    # end
     
   end
   
   
-  # context 'the cash register was initialized with an employee discount' do
-  #     it 'applies the discount to the total price' do
-  #       cash_register_with_discount.add_item("macbook air", 1000)
-  #       cash_register_with_discount.apply_discount
-  #       expect(cash_register_with_discount.total).to eq(800)
-  #     end
+ 
 
-  #     it 'returns success message with updated total' do
-  #       cash_register_with_discount.add_item("macbook air", 1000)
-  #       expect(cash_register_with_discount.apply_discount).to eq("After the discount, the total comes to $800.")
-  #     end
-
-  #     it 'reduces the total' do
-  #       cash_register.total = 0
-  #       cash_register_with_discount.add_item("macbook air", 1000)
-  #       expect{cash_register_with_discount.apply_discount}.to change{cash_register_with_discount.total}.by(-200)
-  #     end
-  #   end
-
-  #   context 'the cash register was not initialized with an employee discount' do
-  #     it 'returns a string error message that there is no discount to apply' do
-  #       expect(cash_register.apply_discount).to eq("There is no discount to apply.")
-  #     end
 end
