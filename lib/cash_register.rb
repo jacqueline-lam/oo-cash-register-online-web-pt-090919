a642ce9ce3864c38fb21afc7abbf3f4fecd78a0d
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-09 22:54:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @total += ( price * quantity)
+    @total += (price * quantity)
     quantity.times { @items << title }
     self.last_trans = price
   end
